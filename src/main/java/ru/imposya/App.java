8334709264ed5cedcef4d6802ad807f4cbd5727e
Alexander Latyshev <imposya0@gmail.com>
2023-02-23 15:50:01 +0300

    Init commit

---
+++ src/main/java/ru/imposya/App.java
@@ -0,0 +1,13 @@
+package ru.imposya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
